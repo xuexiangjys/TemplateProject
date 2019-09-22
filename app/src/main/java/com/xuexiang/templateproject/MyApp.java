a012cd19bc4a1fff09d7bce73fc97aefccff5582
xuexiangjys <xuexiangjys@163.com>
2019-09-23 00:10:54 +0800

    升级版本库

--- app/src/main/java/com/xuexiang/templateproject/MyApp.java
+++ app/src/main/java/com/xuexiang/templateproject/MyApp.java
@@ -26,6 +26,7 @@ import com.xuexiang.xpage.AppPageConfig;
 import com.xuexiang.xpage.PageConfig;
 import com.xuexiang.xpage.PageConfiguration;
 import com.xuexiang.xpage.model.PageInfo;
+import com.xuexiang.xrouter.launcher.XRouter;
 import com.xuexiang.xutil.XUtil;
 import com.xuexiang.xutil.common.StringUtils;
 import com.xuexiang.xutil.tip.ToastUtils;
@@ -70,5 +71,12 @@ public class MyApp extends Application {
             }
 
         });
+
+        // 这两行必须写在init之前，否则这些配置在init过程中将无效
+        if (BuildConfig.DEBUG) {
+            XRouter.openLog();     // 打印日志
+            XRouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        XRouter.init(this);
     }
 }
